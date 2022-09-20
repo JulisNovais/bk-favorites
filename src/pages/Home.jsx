07bde362a65ab9c0a08d02b6fa4bc296c02ee983
prof-otavio-medeiros <alessandrofeitoza01@gmail.com>
2022-09-20 20:54:05 -0300

    subindo com tabela e axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,37 @@
+import axios from 'axios';
 import React from 'react'
+import { useState } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next'
 
 export default function Home() {
+    const [cidades, setCidades] = useState([])
+    async function getBooks() {
+        try {
+            const response = await axios({
+                method: "get",
+                url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados/CE/municipios",
+            })
+    
+            setCidades(response.data);
+            console.log(cidades)
+        } catch (error) {
+            console.log(error);
+        }
+       
+    }
+
+    getBooks();
+
+    const colunas = [{
+        dataField: 'id',
+        text: 'ID'
+    }, {
+        dataField: 'nome',
+        text: 'cidade'
+    }];
+
     return (
-        <h1>Home</h1>
+        <BootstrapTable keyField="id" data={cidades} columns={colunas} />
     )
 }
 
